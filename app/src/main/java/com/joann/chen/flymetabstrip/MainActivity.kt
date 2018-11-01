a3ab74bca44a55d1f42cddc2a408c621a8236aad
JoannChen <yongzuo_chen@dingyuegroup.cn>
2018-11-01 15:03:38 +0800

    修改配置信息

---
+++ app/src/main/java/com/joann/chen/flymetabstrip/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.joann.chen.flymetabstrip
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val titles = arrayOf("男生", "女生")
+    private lateinit var mRecommendPageAdapter: RecommendPageAdapter
+
+    // 男频
+    val manRecommendFragment: Fragment by lazy {
+        Fragment()
+    }
+
+    // 女频
+    val girlRecommendFragment: Fragment by lazy {
+        Fragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mRecommendPageAdapter = RecommendPageAdapter(supportFragmentManager)
+        viewPage.adapter = mRecommendPageAdapter
+        tabStrip.setViewPager(viewPage)
+
+        viewPage.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                viewPage.currentItem = position
+            }
+
+        })
+    }
+
+
+    private inner class RecommendPageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getCount() = titles.size
+
+        override fun getItem(position: Int): Fragment = when (position) {
+            0 -> manRecommendFragment
+            1 -> girlRecommendFragment
+            else -> manRecommendFragment
+        }
+
+        override fun getItemPosition(`object`: Any): Int = PagerAdapter.POSITION_NONE
+
+        override fun getPageTitle(position: Int): CharSequence = titles[position]
+    }
+
+}
